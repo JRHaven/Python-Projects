21a53183dc553485a9e685859c3bbbbdf70a3a77
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 20:12:27 -0500

    Update ls.py

--- Bash For Windows/ls.py
+++ Bash For Windows/ls.py
@@ -1,16 +1,27 @@
+# ls is a basic script that either outputs the files in a directory or returns a list of files in the current
+# directory
+
+# Libraries
 from os import walk
 import os
 import glob
 import systemvariables
+
+# Function that prints out all files 1 by 1 virtically without any other characters on the screen
 def show():
     dir_list = os.listdir(os.getcwd())
     i = 0
     for dirs in dir_list:
         print(dir_list[i])
         i += 1
+        
+# Function that returns a list of everything in the current directory
 def list():
     theList = os.listdir(os.getcwd())
     return theList
+
+# Function that prints out all of the variables, unlike the echo script that only prints out 1 requested
+# variable
 def vars():
     print("usrsession:", systemvariables.usrsession)
     print("HOME:", systemvariables.HOME)
