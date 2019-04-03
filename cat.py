c18c21c4b8c0fc9028d3a7d82817377624a460e2
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 19:36:49 -0500

    Delete cat.py

--- cat.py
+++
@@ -1,8 +0,0 @@
-import os
-def show(file):
-    if(os.path.exists(file) == False):
-        print("cat: " + file + " file not found")
-    else:
-         show = open(file)
-         print(show.read())
-         show.close()
