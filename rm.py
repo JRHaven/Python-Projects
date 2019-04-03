d646d9f899f334729ba31d2717869922ee4b7c5a
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 19:39:14 -0500

    Delete rm.py

--- rm.py
+++
@@ -1,8 +0,0 @@
-import os
-import ls
-def remove(file):
-    directory = ls.list()
-    if(any(file in s for s in directory)):
-        os.remove(file)
-    else:
-        print("rm: The file", file, "does not exist so not deleting!")
