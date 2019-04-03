18b1ab120d7b587ccf01bfe930c004eeef20ce96
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 20:09:03 -0500

    Update filechk.py

--- Bash For Windows/filechk.py
+++ Bash For Windows/filechk.py
@@ -1,4 +1,9 @@
+# Code is used often, so I created a library for it. If the requested file exists, return True. If not, return False
+
+# Uses the ls library to get list of all the files in the directory to look through
 import ls
+
+# Main function
 def check(filename):
     directory = ls.list()
     if(any(filename in s for s in directory)):
