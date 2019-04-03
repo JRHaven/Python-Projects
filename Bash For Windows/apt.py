ad64f3c65bc04a898514b71eacbec76f1e5ef5a7
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 19:53:55 -0500

    Update apt.py

--- Bash For Windows/apt.py
+++ Bash For Windows/apt.py
@@ -1,13 +1,23 @@
+# Apt tries to get a file from the internet but does not work yet
+
+# Libraries
 import os
 import systemvariables
 import urllib
+
+# Main function
 def install(go):
     print("\nMoving to Downloads Directory...")
     os.chdir(systemvariables.HOME + "\\" + systemvariables.usrsession + "\\Downloads")
+    # Commented out lines are for debugging purposes only
     #print(os.getcwd())
     print("Getting Executable from selected source...")
     file = input("What do you want to name this new file? ")
+    
+    # Attempt to retrieve the file
     urllib.request.urlretrieve(go, file)
+    
+    # Ask the user if they want to execute the downloaded file
     confirm = input("Do you want to execute this file[Y,n]? ")
     if((confirm == "Y") or (confirm == "y")):
         os.system(file)
