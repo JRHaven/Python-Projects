761f8a9df468b067a1b18db938ed7799beae150b
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 20:21:37 -0500

    Update rm.py

--- Bash For Windows/rm.py
+++ Bash For Windows/rm.py
@@ -1,7 +1,14 @@
+# rm is a basic script that removes a file
+
+# Libraries
 import os
 import ls
+
+# Main Function. First get a listing of the directory from the ls script
 def remove(file):
     directory = ls.list()
+    
+    # If it exists, remove it. If not, display a message
     if(any(file in s for s in directory)):
         os.remove(file)
     else:
