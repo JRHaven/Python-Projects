70ae6fe8423f91f03724172cc312fd05ad343e98
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 19:59:12 -0500

    Update cat.py

--- Bash For Windows/cat.py
+++ Bash For Windows/cat.py
@@ -1,4 +1,9 @@
+# Cat is a basic script to show the contents of a file
+
+# Only need one library
 import os
+
+# Main Function. First check if the file exists
 def show(file):
     if(os.path.exists(file) == False):
         print("cat: " + file + " file not found")
