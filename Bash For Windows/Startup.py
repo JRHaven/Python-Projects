424daf5c81a1b600d5344d361fa2f664f880836f
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 19:51:34 -0500

    Update Startup.py

--- Bash For Windows/Startup.py
+++ Bash For Windows/Startup.py
@@ -1,3 +1,6 @@
+# Startup is the file that starts off Bash For Windows. You can only run Bash for Windows with this script.
+
+# Import libraries
 import platform
 import bash
 import os
@@ -7,22 +10,33 @@ import usrmgr
 import usrmgr2
 import repair
 import systemvariables
+
+# The commented out lines are for me to use for debugging purposes only.
 #print(os.getcwd())
 #print(os.path.dirname(os.path.realpath(__file__)))
+
+# Set a system variable
 systemvariables.exepath = os.path.dirname(os.path.realpath(__file__))
+
+# Check to make sure we are running on Windows and if not start bash
 if(platform.system() != "Windows"):
     print("Bash for Windows has seen that you are not using Windows. Launching Bash...")
     os.system("bash")
     exit()
+    
+# Move to the root of the file structure
 os.chdir(systemvariables.exepath + "/../..")
 #print(os.getcwd())
+
+# Tell the login prompt what folder we are in
 usrmgr2.usrcheck(os.getcwd())
 username = open("Settings/ivhzadgz.bws" , "r")
+
+# See if any folders are deleted and if they are attempt to fix it using the repair script
 if(os.path.exists("Bash") == False):
     choice = input("Unfortunatly, Bash for Windows could not find your data. Do you want to try to fix this with Bash for Windows Repair? [y, N] # ")
     if((choice == "y") or (choice == "Y")):
         repair.baserepair()
-        bre
     else:
         print("Abort.")
 elif(os.path.exists("Bash/Users") == False):
@@ -37,4 +51,6 @@ elif(os.path.exists("Bash/Users/" + username.read()) == False):
         repair.baseusrfilerepair()
     else:
         print("Abort.")
+        
+# Call the login prompt
 usrmgr.logon()
