f4fcc3c0baf9a5a6bf3c5444f889e385a5464ffc
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 20:00:51 -0500

    Update cd.py

--- Bash For Windows/cd.py
+++ Bash For Windows/cd.py
@@ -1,5 +1,10 @@
+# cd is a basic script that deciphers tildas and slashes then goes into desired directory
+
+# Libraries
 import os
 import systemvariables
+
+# Main Function
 def go(path):
     if(path == "~"):
         os.chdir(systemvariables.HOME)
