77b607ca6464787877df9be65400e031b31a83cd
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 20:15:37 -0500

    Update nano.py

--- Bash For Windows/nano.py
+++ Bash For Windows/nano.py
@@ -1,8 +1,17 @@
+# nano is a basic script that opens a file in notepad. The nano script does NOT try to imitate the real GNU Nano
+# text editor.
+
+# Libraries
 import os
 import touch
 import ls
+
+# Main Function. First get a listing of the directory using the ls script
 def write(filename):
     theList = ls.list()
+    
+    # Find out if there is a file called the same as the input text. If not, create the file using the touch
+    # script before opening notepad
     if(any(filename in s for s in theList)):
         os.system("notepad " + filename)
     else:
