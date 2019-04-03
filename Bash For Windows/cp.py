fadfc73ffae3d6aa4015acde279430d8514656cb
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 20:03:10 -0500

    Update cp.py

--- Bash For Windows/cp.py
+++ Bash For Windows/cp.py
@@ -1,7 +1,13 @@
+# cp is a basic script that copies a file
+
+# Libraries
 import filechk
 import os
+
+# Main Function. First checks if the file wanted exists
 def copy(srcfile, dstfile):
     if(filechk.check(srcfile) == True):
+        # Read the file, copy the contents to a new file, then changes the name and/or moves it to new directory
         source = open(srcfile, "r")
         copy = open(srcfile + " - copy", "w")
         copy.write(source.read())
