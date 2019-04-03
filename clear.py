2f8f77a1ef1d98e8e78bda778d4694a306ee6551
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 19:13:42 -0500

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,5 +1,11 @@
-import platform
-import os
+# clear makes it easier to clear the screen based on what platform one is on
+
+# We need 2 libraries
+import platform, os
+
+# The library that makes it happen. If we are running Windows, run cls for
+# the command prompt. If we are running Mac or Linux, which uses bash for the
+# most part, run the clear command
 def cs():
     if(platform.system() == "Windows"):
         os.system("cls")
