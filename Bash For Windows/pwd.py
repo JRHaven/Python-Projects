00f2ea0989fcd9b8228c31284285bd1ac65365cb
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 20:16:55 -0500

    Update pwd.py

--- Bash For Windows/pwd.py
+++ Bash For Windows/pwd.py
@@ -1,3 +1,8 @@
+# pwd is a basic 3-lined script that simply returns the path of the directory we are currently in
+
+# Our only library
 import os
+
+# Main Function
 def get():
     return os.getcwd()
