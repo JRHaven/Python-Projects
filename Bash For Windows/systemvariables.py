98457c3801a0a9894408910cb3118a9bc26e3150
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-02 20:24:00 -0500

    Update systemvariables.py

--- Bash For Windows/systemvariables.py
+++ Bash For Windows/systemvariables.py
@@ -1,3 +1,7 @@
+# systemvariables holds all variables open to all parts of the system who import them. The variables can be set, modified,
+# and read depending on what a script may need to do with them. The end user may only see what are in the variables through
+# means of the lsvars and echo commands.
+
 usrsession = "" 
 HOME = ""
 ROOT = ""
