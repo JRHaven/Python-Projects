2e0c6d0f34cabe1197bb748bf908096fe8c48654
Jeremiah Haven <49214876+JR-Tech-and-Software@users.noreply.github.com>
2023-02-21 21:01:11 -0600

    Why were the comments like // instead of #
    I haven't used this in a very long time, and this shows. It's been using comments as // instead of # for how many years and I never noticed.

--- filetts.py
+++ filetts.py
@@ -1,7 +1,7 @@
 '''
 This script is under the MIT License.
 
-Copyright 2019 Jeremiah Haven
+Copyright 2019-2023 Jeremiah Haven
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
 files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
@@ -16,36 +16,36 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER I
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-// Use the gtts library to create the audio file
+# Use the gtts library to create the audio file
 from gtts import gTTS
 
-// Use the os library so that we can interact with the file
+# Use the os library so that we can interact with the file
 import os
 
-// Get an input of the file from the user
+# Get an input of the file from the user
 file = input("Read from file: ")
 
-// View the contents of the file with a variable
+# View the contents of the file with a variable
 contents = open(file)
 
-// Store the text of the file in this variable
+# Store the text of the file in this variable
 say = contents.read()
 
-// It takes a while for the audio file to be created. Infrom the end user
+# It takes a while for the audio file to be created. Infrom the end user
 print("\nGenerating speech. Be patient. This could take a while...\n")
 
-// Start generating the file. I assume that this is the english language
+# Start generating the file. I assume that this is the english language
 tts = gTTS(text=say, lang='en')
 
-// File to save to
+# File to save to
 tts.save("text.mp3")
 
-// Make sure the vlc media player isn't already running
+# Make sure the vlc media player isn't already running
 os.system("killall cvlc")
 
-// Play the file with the command line front of the vlc media player. Modify this file if
-// you cannot use this command
+# Play the file with the command line front of the vlc media player. Modify this file if
+# you cannot use this command
 os.system("mpv text.mp3")
 
-// Delete the file
+# Delete the file
 os.remove("text.mp3")
